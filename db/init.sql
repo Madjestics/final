f096ada13a593eedc7436bcc4c1fc4e51f4f9d2c
madjestics <dmitri10248@gmail.com>
2024-03-23 20:29:18 +0500

    добавлен README.md и запуск через docker-compose

---
+++ db/init.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS public.director
+(
+    id bigint NOT NULL,
+    fio text NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.movie
+(
+    id bigint NOT NULL,
+    title text NOT NULL,
+    year integer NOT NULL,
+    director bigint NOT NULL,
+    length time without time zone NOT NULL,
+    rating integer NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+ALTER TABLE IF EXISTS public.movie
+    ADD FOREIGN KEY (director)
+    REFERENCES public.director (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
+
+END;
